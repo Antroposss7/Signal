7a9ae17a85a24123776f5cf2634c2a41277c86e2
Antropos7 <ruslanshm@code.edu.az>
2023-10-24 20:32:48 +0400

    First Commit

---
+++ WebSocket-SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebSocket_SignalR.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public async Task AddToGroupAsync(string group)
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, group);
+		}
+
+		public async Task RemoveFromGroupAsync(string group)
+		{
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+		}
+
+		public async Task SendMessage(string username,string group , string message)
+		{
+			await Clients.Group(group).SendAsync("ReceiveMessage", username, message);
+		}
+	}
+}
